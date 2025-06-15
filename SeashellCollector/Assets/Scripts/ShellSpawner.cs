9b434211b88d63f888e6eac8a58b92989466cfc1
Clarkson <graceclarkson1415@gmail.com>
2025-06-15 16:21:05 +1000

    init

---
+++ SeashellCollector/Assets/Scripts/ShellSpawner.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using UnityEngine;
+
+public class ShellSpawner : MonoBehaviour
+{
+    public List<GameObject> Shells;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
