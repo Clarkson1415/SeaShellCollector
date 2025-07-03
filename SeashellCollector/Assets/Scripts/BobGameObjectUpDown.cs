c7d7ad16d8a4c84960448cd8f63ce6ba24670ebe
Clarkson <graceclarkson1415@gmail.com>
2025-07-03 19:33:32 +1000

    lauren tested

--- SeashellCollector/Assets/Scripts/BobGameObjectUpDown.cs
+++ SeashellCollector/Assets/Scripts/BobGameObjectUpDown.cs
@@ -1,3 +1,5 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
@@ -6,6 +8,7 @@ using UnityEngine;
 public class BobGameObjectUpDown : MonoBehaviour
 {
     [Header("Movement Settings")]
+    [SerializeField] bool ApplyToChildren = false;
     public float amplitudeY = 0.5f; // How much to bob up and down
     public float amplitudeX = 0.2f; // Optional side-to-side sway
     public float speed = 1f;        // Speed of the bobbing motion
@@ -22,7 +25,22 @@ public class BobGameObjectUpDown : MonoBehaviour
     {
         float yOffset = Mathf.Sin(Time.time * speed + phaseOffset.y) * amplitudeY;
         float xOffset = Mathf.Cos(Time.time * speed + phaseOffset.x) * amplitudeX;
+        var offsetVector = new Vector3(xOffset, yOffset, 0f);
 
-        transform.localPosition = startPos + new Vector3(xOffset, yOffset, 0f);
+        //List<Transform> originalChildrenTransforms = new();
+        //foreach(Transform child in transform)
+        //{
+        //    originalChildrenTransforms.Add(child);
+        //}
+
+        //transform.localPosition = startPos + offsetVector;
+
+        //if (!ApplyToChildren)
+        //{
+        //    foreach (Transform child in transform)
+        //    {
+        //        child.localPosition -= // set to original before the move
+        //    }
+        //}
     }
 }
